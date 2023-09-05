0d781ca11ba58e363cd1fd5e4453a7c93a0481f2
lapepitedor <messubrinda@gmail.com>
2023-09-05 23:07:03 +0200

    Daten mit fetch holen

---
+++ WebData/fetch.js
@@ -0,0 +1,28 @@
+import http from "node:http";
+import fetch from "node-fetch";
+const graz = {
+  lat: 47.076668,
+  long: 15.421371,
+};
+
+const weatherServer = http.createServer((req, resp) => {
+    let weatherString = "Ich habe keine Wetter-Infos";
+    const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${graz.lat}&longitude=${graz.long}&current_weather=true`;
+        
+    
+    fetch(weatherApiUrl)
+      .then((r) => {
+        return r.json();
+      })
+      .then((weatherObject) => {
+        console.log(weatherObject.current_weather.temperature); // Afficher les données météorologiques dans la console
+        weatherString =
+          "Temperatur in Graz:" + weatherObject.current_weather.temperature;
+
+        resp.write(weatherString);
+        resp.end();
+      });
+});
+weatherServer.listen(4500, () => {
+  console.log("Server is listening on port 4500");
+});
