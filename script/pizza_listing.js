b85be7e7a17bec4d90d4684d3f8ce6720a09697e
lapepitedor <messubrinda@gmail.com>
2023-09-05 21:48:35 +0200

    Template pug Engine nutzen

--- script/pizza_listing.js
+++ script/pizza_listing.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import querystring from "node:querystring";
 import fs from "node:fs";
+import pug from "pug";
 
 const pizzaOffers = [
   { name: "Ananas Dave", cost: 12 },
@@ -10,48 +11,46 @@ const pizzaOffers = [
 const server = http.createServer(function (req, res) {
   res.setHeader("Content-Type", "text/html; charset=utf-8");
 
-    if (req.url === "/bestellung") {
-      // Traiter la soumission du formulaire lors de la requête bestellung
-       
-        let formData = ""; // Créer une variable pour stocker les données du formulaire
-        
-      
-      req.on("data", function (data) {
-        // Écouter les données du formulaire entrantes
-        formData += data;
-      });
-        
-      //// Une fois toutes les données reçues
+  if (req.url === "/bestellung") {
+    // Traiter la soumission du formulaire lors de la requête bestellung
 
-      req.on("end", function () {
-        const bestellt = querystring.decode(formData);
-        res.write("<h1>Bestellseite</h1>");
-        // Utiliser la variable bestellt pour afficher la commande
-         
-          res.write(`Danke für die Bestellung: ${bestellt.bestellung}`);
-        res.end();
-      }); //
-    }
-    else if (req.url === "/") {
-      //start seite
-        let pizzaString = "";
-        pizzaOffers.forEach((pizza) => {
-            pizzaString += `${pizza.name}, ${pizza.cost} EUR <br/>`;
-        });
-        
-            const datei = fs.readFileSync("script/startseite.html").toString();
-        
-        res.write(datei.replace("PIZZA AUFLISTUNG HIER",  pizzaString));
+    let formData = ""; // Créer une variable pour stocker les données du formulaire
 
-       res.end();
-    }
-    else
-    {
-        res.statusCode = 404;
-        res.write("Diese Seite existiert nicht");
-        res.end();
-    }
- 
+    req.on("data", function (data) {
+      // Écouter les données du formulaire entrantes
+      formData += data;
+    });
+
+    //// Une fois toutes les données reçues
+
+    req.on("end", function () {
+      const bestellt = querystring.decode(formData);
+      res.write("<h1>Bestellseite</h1>");
+      // Utiliser la variable bestellt pour afficher la commande
+
+      res.write(`Danke für die Bestellung: ${bestellt.bestellung}`);
+      res.end();
+    }); //
+  } else if (req.url === "/") {
+      const compiledFunction = pug.compileFile("script/startseite.pug");
+      res.write(compiledFunction({ pizzaOffers }));
+      /*
+    //start seite
+    // let pizzaString = "";
+    // pizzaOffers.forEach((pizza) => {
+    //   pizzaString += `${pizza.name}, ${pizza.cost} EUR <br/>`;
+    // });
+
+    // const datei = fs.readFileSync("script/startseite.html").toString();
+
+    // res.write(datei.replace("PIZZA AUFLISTUNG HIER", pizzaString)); */
+
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.write("Diese Seite existiert nicht");
+    res.end();
+  }
 });
 server.listen(3500, () => {
   console.log("Server is listening on port 3500");
